103670f0d6cc49c1457bbcd13225afa8b3819bed
rchola <49288744+rchola@users.noreply.github.com>
2022-05-30 19:56:26 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello World, DevOps has arrived!" to the terminal window.
-        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, SPTECH School are Welcome!");
     }
 
 }
